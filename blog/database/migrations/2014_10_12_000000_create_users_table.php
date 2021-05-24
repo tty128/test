aa82cc0f6b6e35cfadbe72bfa542ec09aa884086
tty128 <83931052+tty128@users.noreply.github.com>
2021-05-24 12:38:49 +0900

    Update 2014_10_12_000000_create_users_table.php

--- blog/database/migrations/2014_10_12_000000_create_users_table.php
+++ blog/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('name',60);
+            $table->string('nickname',60)->nullable();
+            $table->string('email',200)->unique();
+            $table->timestamp('email_verified_at',200)->nullable();
+            $table->string('password',16);
             $table->timestamps();
             $table->rememberToken();
         });
