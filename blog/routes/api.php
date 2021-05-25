26977f33fac137ce7d52fae973e7ca6ac4e86694
tty128 <83931052+tty128@users.noreply.github.com>
2021-05-25 11:37:23 +0900

    Update api.php

--- blog/routes/api.php
+++ blog/routes/api.php
@@ -19,3 +19,6 @@ Route::post('register', 'API\UserController@register')->name('api_register');
 Route::middleware('auth:api')->get('details', 'API\UserController@details');
 Route::get('/post', 'PostController@apiIndex')->name('posts');
 Route::get('/post/{post}', 'PostController@show')->name('postshow');
+
+// 今回は外部APIでPOSTたたかないため関係ないが、一応apiでPOSTした場合のチェックしてみたい。
+Route::middleware('auth:api')->post('/post/create', 'PostController@store')->name('api_postcreate'); //未チェック
