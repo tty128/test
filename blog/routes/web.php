ecd32c56076f813b5f5f8beb2a37a919d6abe369
tty128 <83931052+tty128@users.noreply.github.com>
2021-05-25 14:26:46 +0900

    Update web.php

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -11,13 +11,24 @@
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/post', 'PostController@index')->name('post');
-Route::middleware('auth')->post('/post/create', 'PostController@store')->name('postcreate');
 
+Route::middleware('auth')->group(['prefix' => 'post' , 'as' => 'post.'],function(){
+    Route::get('/', 'PostController@index')->name('all');
+    // Route::get('/edit','PostController@edit')->name('edit'); // vue.componentで実装予定    
+    
+    Route::post('/create', 'PostController@store')->name('create');
+
+    // 未チェック
+    Route::post('/update/{id}', 'PostController@update')->name('update');
+    Route::post('/delete/{id}', 'PostController@destroy')->name('delete');
+
+});
