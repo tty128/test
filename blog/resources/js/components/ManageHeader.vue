ca3e9e6d07cef5a521aa8b047a6540fb380cb4a9
tty128 <83931052+tty128@users.noreply.github.com>
2021-06-09 16:05:29 +0900

    Add files via upload

--- blog/resources/js/components/ManageHeader.vue
+++ blog/resources/js/components/ManageHeader.vue
@@ -1,65 +1,54 @@
 <template>
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
+    <header class="manage__header">
+        <div class="wrapper">
             <router-link
-                    :to="{path:'/manage'}"
+                    :to="{path:'/'}"
                     tag="a"
-                    class="navbar-brand"
+                    id="manage__Logo"
             >
                 {{ appName }}
             </router-link>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto"></ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-
-                    <li class="nav-item dropdown">
-                        <a
-                            id="navbarDropdown"
-                            class="nav-link dropdown-toggle"
-                            href="#"
-                            role="button"
-                            data-toggle="dropdown"
-                            aria-haspopup="true"
-                            aria-expanded="false"
+            <div class="container">
+                <ul class="manage__header_ul">
+                    <li>
+                        <input type="checkbox" name="username" id="Button__UserName">
+                        <label for="Button__UserName"
+                            class="label_button"
                         >
                             {{ userName }}
                             <span class="caret"></span>
-                        </a>
+                        </label>
 
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a
-                                class="dropdown-item"
-                                :href="routeLogout"
-                                @click="logoutButton"
-                            >
-                                LOGOUT
-                            </a>
-
-                            <form id="logout-form" :action="route_logout" method="POST" style="display: none;">
-                                <laravel-vue-csrf
-                                    :csrf="CSRFtoken"
+                        <ul class="manage__dropdown_menu">
+                            <li>
+                                <a
+                                    onclick="event.preventDefault();document.getElementById('logout-form').submit();"
                                 >
-                                </laravel-vue-csrf>
-                            </form>
-                        </div>
+                                    LOGOUT
+                                </a>
+
+                                <form id="logout-form" :action="route_logout" method="POST" style="display: none;">
+                                    <!-- <laravel-vue-csrf
+                                        :csrf="CSRFtoken"
+                                    ></laravel-vue-csrf> -->
+                                </form>
+                            </li>
+                        </ul>
                     </li>
                 </ul>
             </div>
         </div>
-    </nav>
+    </header>
 </template>
 
 <script>
-    export default {
+    module.exports={
         props:{
             app_name:String,
             user_name:String,
             route_logout:String,
-            csrf:Array
+            csrf:Array,
         },
         data: function() {
             return{
@@ -79,41 +68,132 @@
                 return this.csrf
             },
         },
-        methods:{
-            logoutButton:function(){
-                event.preventDefault()
-                document.getElementById('logout-form').submit();
-            }
-        }
     }
 </script>
 
 <style scoped>
-    nav {
-        position:relative;
 
-        display:flex;
-        align-items:center;
-        justify-content:flex-start;
+header.manage__header{
+    color:white;
+    background: red;
+}
+header.manage__header >.wrapper >.container{background : green;}
+#manage__Logo{background: blue;}
+.manage__header_ul .manage__dropdown_menu{background: cornflowerblue;}
+.manage__header_ul .label_button{background: coral;}
 
-        width:100%;
-        height:50px;
 
-        background:gray;
-    }
 
-    nav > div {
-        display: flex;
-        align-items: center;
-        justify-content: center;
+header.manage__header{
+    z-index: 100;
 
-        width: 19%;
-        height: 100%;
+    position: fixed;
 
-        color: white;
+    width:100%;
+    height: 5rem;
+}
 
-        font-size: 1.4rem;
-        text-decoration: none;
-    }
+header.manage__header *{
+    list-style: none;
 
+    margin: 0;
+    padding:0;
+}
+
+header.manage__header a:link,
+header.manage__header a:visited{
+    color:inherit;
+    text-decoration: none;
+}
+
+.manage__header>.wrapper{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    width: 90%;
+    height: 100%;
+
+    margin:0 auto;
+}
+
+#manage__Logo{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    min-width: 8rem;
+    height: 100%;
+
+
+}
+
+header.manage__header >.wrapper >.container{
+    height: 100%;
+
+    float: right;
+}
+
+.manage__header_ul{
+    user-select:none;
+
+    height: 100%;
+}
+
+.manage__header_ul > li{
+    height: 100%;
+    width:100%;
+}
+
+.manage__header_ul input{
+    display: none;
+}
+
+.manage__header_ul .manage__dropdown_menu{
+    z-index: 0;
+
+    pointer-events: none;
+
+    position: relative;
+    top:-100%;
+
+    height: 100%;
+
+    opacity: 0;
+
+    transition:all 0.3s;
+}
+
+.manage__header_ul .manage__dropdown_menu li{
+    height: 100%;
+}
+
+.manage__header_ul .manage__dropdown_menu a{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    height: 100%;
+}
+
+.manage__header_ul .label_button{
+    z-index: 10;
+
+    position: relative;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width:100%;
+    min-width: 7.5rem;
+    height: 100%;
+}
+
+input#Button__UserName:checked ~ .manage__dropdown_menu{
+    cursor: pointer;
+    pointer-events: all;
+    opacity: 1;
+    transform: translateY(100%);
+}
 </style>
