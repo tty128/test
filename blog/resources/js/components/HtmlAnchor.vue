67173e5231dd5a5453bd9e6c9472122a26113e1c
tty128 <83931052+tty128@users.noreply.github.com>
2021-06-25 14:55:37 +0900

    Add files via upload

---
+++ blog/resources/js/components/HtmlAnchor.vue
@@ -0,0 +1,97 @@
+﻿<template>
+    <div id="HtmlAnchor"
+        v-if='autoAnchor'
+    >
+        <html-anchor-list
+            :items='autoAnchor'
+        ></html-anchor-list>
+    </div>
+</template>
+
+<script>
+// import HtmlAnchorList from './HtmlAnchorList.vue';
+// export default {
+module.exports = {
+    // components:{
+    //     HtmlAnchorList
+    // },
+    props:{
+        dom_string:{type:[String],default:''},
+        query_select:{type:[String],default:"h1,h2,h3"}
+    },
+    computed:{
+        autoAnchor:function(){
+            if(this.dom_string !== ''){
+                const doc = new DOMParser().parseFromString(this.dom_string, "text/html");
+                const querys = doc.querySelectorAll(this.query_select)
+
+                if (querys.length > 0) {
+                    // セレクターの種類を配列に変換し重複を排除する
+                    const queryArrayToSet = this.query_select.split(',')
+                    const queryArray = [...new Set(queryArrayToSet)]
+
+                    // returnする配列の初期化
+                    let resultArr = []
+
+                    // 配列への参照：ネストされるたび参照を代入する
+                    let resultRef = resultArr;
+
+                    // 代入先のObjectに
+                    let isCreatedArr = []
+                    queryArray.forEach(element => {isCreatedArr.push(false)})
+
+                    querys.forEach(query => {
+                        // id無しのエラーが存在する場合は無視
+                        if(query.id !== null && query.id != "" ){
+                            let qTag = query.tagName.toLowerCase()
+                            let nestIndex = queryArray.findIndex(elem => elem == qTag)
+                            for(let i = 0 ; i < queryArray.length ; i++){
+                                if ( i == nestIndex ) {
+                                    resultRef.push({
+                                        tag:qTag,
+                                        text:query.id,
+                                        children:[]
+                                    })
+                                    isCreatedArr[i] = true
+                                    resultRef = resultArr
+
+                                } else if ( i < nestIndex ) {
+                                    // タグが飛んでる場合（h1の次にh2を飛ばしてh3が来てる等）は空のObjectを作成する
+                                    if(!isCreatedArr[i]){
+                                        resultRef.push({
+                                            tag:'',
+                                            text:'',
+                                            children:[]
+                                        })
+                                        isCreatedArr[i] = true
+                                    }
+                                    // 参照先次の入れ子に変更
+                                    resultRef = resultRef[resultRef.length - 1].children
+                                } else {
+                                    isCreatedArr[i] = false
+                                }
+                            }
+
+                        }
+                    })
+
+                    return resultArr
+                }
+            }
+
+            return null
+        }
+    },
+}
+</script>
+
+<style scoped>
+    #HtmlAnchor{
+        background: cadetblue;
+        width: 100%;
+        height: auto;
+
+        padding: 1rem;
+        border-radius: 1rem;
+    }
+</style>
---
+++ blog/resources/js/components/HtmlAnchorList.vue
@@ -0,0 +1,73 @@
+﻿<template>
+    <ul :class="['anchor-ul', 'anchor-level__' + getLevel ]">
+        <li
+            class="anchor-li"
+            v-for="item in items"
+            :key="item.text"
+        >
+            <a
+                v-if="item.text != ''"
+                :class="'tag-' + item.tag"
+                @click="scrollToAnchorPoint('#' + item.text )"
+            >
+                {{ item.text }}
+            </a>
+            <html-anchor-list
+                v-if="item.children.length"
+                :items="item.children"
+                :level="getLevel"
+            ></html-anchor-list>
+        </li>
+    </ul>
+</template>
+
+<script>
+//import HtmlAnchorList from './HtmlAnchorList.vue'
+// export default {
+module.exports = {
+    // name:html-anchor-list,
+    // components:{
+    //     HtmlAnchorList
+    // },
+    props:{
+        items:Array,
+        level:{type:[Number],default:0}
+    },
+    computed:{
+        getLevel:function(){
+            return this.level + 1
+        }
+    },
+    methods:{
+        scrollToAnchorPoint(ref) {
+            const elem = document.querySelector(ref)
+            elem.scrollIntoView({ behavior: 'smooth' , block: 'center'})
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .anchor-ul{
+        list-style: none;
+        margin-left: 1.4rem;
+    }
+
+    .anchor-ul a{
+        cursor: pointer;
+    }
+
+    .anchor-level__1{
+        margin-left: 0;
+        font-size: 1.8rem ;
+    }
+
+    .anchor-level__2{
+        font-size: 1.6rem ;
+    }
+
+    .anchor-level__3{
+        font-size: 1.4rem ;
+    }
+
+</style>
