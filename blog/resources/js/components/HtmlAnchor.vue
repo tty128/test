2ce47284b6a60a333f00a3ed0237fef47173747d
tty128 <83931052+tty128@users.noreply.github.com>
2021-06-29 15:08:37 +0900

    Add files via upload

--- blog/resources/js/components/HtmlAnchor.vue
+++ blog/resources/js/components/HtmlAnchor.vue
@@ -2,9 +2,17 @@
     <div id="HtmlAnchor"
         v-if='autoAnchor'
     >
-        <html-anchor-list
-            :items='autoAnchor'
-        ></html-anchor-list>
+        <keep-alive>
+            <ul class="anchor-ul anchor-level__1">   
+                <html-anchor-list
+                    v-for="items in autoAnchor"
+                    :key="items.id"
+
+                    :item='items'
+                    :is_button="is_button"
+                ></html-anchor-list>
+            </ul>
+        </keep-alive>
     </div>
 </template>
 
@@ -17,7 +25,8 @@ module.exports = {
     // },
     props:{
         dom_string:{type:[String],default:''},
-        query_select:{type:[String],default:"h1,h2,h3"}
+        query_select:{type:[String],default:"h1,h2,h3"},
+        is_button:{type:[Boolean],default:true}
     },
     computed:{
         autoAnchor:function(){
@@ -25,6 +34,7 @@ module.exports = {
                 const doc = new DOMParser().parseFromString(this.dom_string, "text/html");
                 const querys = doc.querySelectorAll(this.query_select)
 
+                // クエリ判定
                 if (querys.length > 0) {
                     // セレクターの種類を配列に変換し重複を排除する
                     const queryArrayToSet = this.query_select.split(',')
@@ -36,7 +46,7 @@ module.exports = {
                     // 配列への参照：ネストされるたび参照を代入する
                     let resultRef = resultArr;
 
-                    // 代入先のObjectに
+                    // 代入先のObjectが作成済みか判定
                     let isCreatedArr = []
                     queryArray.forEach(element => {isCreatedArr.push(false)})
 
@@ -45,38 +55,46 @@ module.exports = {
                         if(query.id !== null && query.id != "" ){
                             let qTag = query.tagName.toLowerCase()
                             let nestIndex = queryArray.findIndex(elem => elem == qTag)
+
+                            // メイン処理
                             for(let i = 0 ; i < queryArray.length ; i++){
                                 if ( i == nestIndex ) {
                                     resultRef.push({
                                         tag:qTag,
-                                        text:query.id,
+                                        id:query.id,
+                                        text:query.innerHTML,
                                         children:[]
                                     })
                                     isCreatedArr[i] = true
+
+                                    //参照先を初期化
                                     resultRef = resultArr
 
                                 } else if ( i < nestIndex ) {
                                     // タグが飛んでる場合（h1の次にh2を飛ばしてh3が来てる等）は空のObjectを作成する
                                     if(!isCreatedArr[i]){
-                                        resultRef.push({
-                                            tag:'',
-                                            text:'',
-                                            children:[]
+                                        resultRef.push({ 
+                                            children:[] 
                                         })
                                         isCreatedArr[i] = true
                                     }
                                     // 参照先次の入れ子に変更
                                     resultRef = resultRef[resultRef.length - 1].children
+
                                 } else {
                                     isCreatedArr[i] = false
                                 }
+
                             }
+                            // メイン処理ここまで
 
                         }
                     })
 
                     return resultArr
                 }
+                // クエリ判定ここまで
+
             }
 
             return null
@@ -91,7 +109,97 @@ module.exports = {
         width: 100%;
         height: auto;
 
-        padding: 1rem;
+        padding: 1.6rem;
         border-radius: 1rem;
+
+        word-break: normal;
+    }
+
+    /* .anchor-li{
+        margin-top: 0.2rem;
+    } */
+
+    .anchor-ul{
+        list-style: none;
+        user-select: none;
+
+        margin-left: 1.4rem;
+    }
+
+    .anchor-ul a{
+        cursor: pointer;
+        word-break: break-word;
+    }
+
+    .anchor-level__1{
+        margin: 0;
+        font-size: 1.8rem ;
+    }
+    .anchor-level__1 > li {
+        margin-top: 0.6rem;
+    }
+    .anchor-level__1 > li:first-child {
+        margin-top: 0;
+    }
+
+    .anchor-level__1 > li > span.wrapper{
+        display: inline-block;
+        width: 100%;
+
+        border-bottom: dotted 1px;
+    }
+    .anchor-level__1 > li > span.wrapper a{
+        margin-left: 1rem;
+    }
+
+    .anchor-level__2{
+        list-style: disc;
+        margin-left: 3.6rem;
+        font-size: 1.6rem ;
+    }
+
+    .anchor-level__2 > li {
+        margin-top: 0.5rem;
+    }
+
+    .anchor-level__3{
+        list-style: circle;
+        margin-left: 1.8rem;
+        font-size: 1.4rem ;
+    }
+    .anchor-level__3 > li {
+        margin-top: 0.4rem;
+    }
+
+    .anchor-button{
+        vertical-align: middle;
+    }
+    .anchor-button svg{
+        width: 1.5rem;
+        height: 1.5rem;
+
+        margin-left: 0.35rem;
+
+        stroke: inherit;
+        fill: inherit;
+
+        transition: all 0.3s;
+    }
+
+    .anchor-button:hover{
+        stroke: blueviolet;
+        fill: blueviolet;
+    }
+
+    .anchor-button-enter-active {
+    transition: all .3s ease;
+    }
+    .anchor-button-leave-active {
+    transition: all .5s ease;
+    }
+    .anchor-button-enter , .anchor-button-leave-to 
+    {
+        transform: translateX(1rem);
+        opacity: 0;
     }
 </style>
--- blog/resources/js/components/HtmlAnchorList.vue
+++ blog/resources/js/components/HtmlAnchorList.vue
@@ -1,28 +1,46 @@
 ﻿<template>
-    <ul :class="['anchor-ul', 'anchor-level__' + getLevel ]">
-        <li
-            class="anchor-li"
-            v-for="item in items"
-            :key="item.text"
-        >
-            <a
-                v-if="item.text != ''"
-                :class="'tag-' + item.tag"
-                @click="scrollToAnchorPoint('#' + item.text )"
-            >
-                {{ item.text }}
-            </a>
-            <html-anchor-list
-                v-if="item.children.length"
-                :items="item.children"
-                :level="getLevel"
-            ></html-anchor-list>
+        <li class="anchor-li">
+            <span class="wrapper">
+                <a
+                    v-if="!isEmptyItemText"
+                    :class="'tag-' + item.tag"
+                    @click="scrollToAnchorPoint('#' + item.id )"
+                >
+                    {{ item.text }}
+                </a>
+                <span v-if="isEmptyItemText && isButton" @click="buttonClick()">[{{ isButtonOn ? 'close':'open' }}]</span>
+                <span
+                    class="anchor-button"
+                    v-if="isExistsChildren && isButton"
+                    @click="buttonClick()"
+                >
+                    <svg v-if="!isButtonOn" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11 14.5V16.5H13V14.5H15V12.5H13V10.5H11V12.5H9V14.5H11Z" /><path fill-rule="evenodd" clip-rule="evenodd" d="M4 1.5C2.89543 1.5 2 2.39543 2 3.5V4.5C2 4.55666 2.00236 4.61278 2.00698 4.66825C0.838141 5.07811 0 6.19118 0 7.5V19.5C0 21.1569 1.34315 22.5 3 22.5H21C22.6569 22.5 24 21.1569 24 19.5V7.5C24 5.84315 22.6569 4.5 21 4.5H11.874C11.4299 2.77477 9.86384 1.5 8 1.5H4ZM9.73244 4.5C9.38663 3.9022 8.74028 3.5 8 3.5H4V4.5H9.73244ZM3 6.5C2.44772 6.5 2 6.94772 2 7.5V19.5C2 20.0523 2.44772 20.5 3 20.5H21C21.5523 20.5 22 20.0523 22 19.5V7.5C22 6.94772 21.5523 6.5 21 6.5H3Z"/></svg>
+                    <svg v-if="isButtonOn" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 14.5V12.5H15V14.5H9Z" fill="currentColor" /><path fill-rule="evenodd" clip-rule="evenodd" d="M4 1.5C2.89543 1.5 2 2.39543 2 3.5V4.5C2 4.55666 2.00236 4.61278 2.00698 4.66825C0.838141 5.07811 0 6.19118 0 7.5V19.5C0 21.1569 1.34315 22.5 3 22.5H21C22.6569 22.5 24 21.1569 24 19.5V7.5C24 5.84315 22.6569 4.5 21 4.5H11.874C11.4299 2.77477 9.86384 1.5 8 1.5H4ZM9.73244 4.5C9.38663 3.9022 8.74028 3.5 8 3.5H4V4.5H9.73244ZM3 6.5C2.44772 6.5 2 6.94772 2 7.5V19.5C2 20.0523 2.44772 20.5 3 20.5H21C21.5523 20.5 22 20.0523 22 19.5V7.5C22 6.94772 21.5523 6.5 21 6.5H3Z"/></svg>
+                </span>
+            </span>
+            <transition name="anchor-button" mode="out-in">
+                <ul 
+                    :class="['anchor-ul', 'anchor-level__' + getLevel ]"
+                    v-if="isExistsChildren"
+                    v-show="!isButton || isButtonOn "
+                >
+                        <html-anchor-list
+                            v-for="child in item.children"
+                            :key="child.id"
+                            :item="child"
+                            :level="getLevel"
+                            :is_button="is_button"
+                            :btn_nest="btn_nest"
+                            :is_default_open="!(child.text && child.text != '')"
+                        />
+                </ul>
+            </transition>
         </li>
-    </ul>
 </template>
 
 <script>
 //import HtmlAnchorList from './HtmlAnchorList.vue'
+
 // export default {
 module.exports = {
     // name:html-anchor-list,
@@ -30,10 +48,30 @@ module.exports = {
     //     HtmlAnchorList
     // },
     props:{
-        items:Array,
-        level:{type:[Number],default:0}
+        item:Object,
+        level:{type:[Number],default:1},
+        is_button:{type:[Boolean],default:false},
+        btn_nest:{type:[Number],default:1},
+        is_default_open:{type:[Boolean],default:false}
+    },
+    data:function(){
+        return {
+            is_button_on:false,
+        }
     },
     computed:{
+        isExistsChildren:function(){
+            return this.item.children.length ? true :false
+        },
+        isEmptyItemText:function(){
+            return !this.item.text || this.item.text == ''
+        },
+        isButton:function(){
+            return this.is_button && this.level >= this.btn_nest
+        },
+        isButtonOn:function(){
+            return this.is_button_on !== this.is_default_open
+        },
         getLevel:function(){
             return this.level + 1
         }
@@ -42,32 +80,15 @@ module.exports = {
         scrollToAnchorPoint(ref) {
             const elem = document.querySelector(ref)
             elem.scrollIntoView({ behavior: 'smooth' , block: 'center'})
-        }
-    }
+        },
+        buttonClick(){
+            this.is_button_on = this.is_button_on ? false : true
+        },
+    },
 }
 </script>
 
 <style scoped>
-    .anchor-ul{
-        list-style: none;
-        margin-left: 1.4rem;
-    }
 
-    .anchor-ul a{
-        cursor: pointer;
-    }
-
-    .anchor-level__1{
-        margin-left: 0;
-        font-size: 1.8rem ;
-    }
-
-    .anchor-level__2{
-        font-size: 1.6rem ;
-    }
-
-    .anchor-level__3{
-        font-size: 1.4rem ;
-    }
 
 </style>
